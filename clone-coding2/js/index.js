cbc906c98c4063abe8a4a0e0001f5ec8def83f8e
geby9469 <bjk9406@gmail.com>
2022-09-01 02:14:02 +0900

    delete clone-coding practice

--- clone-coding2/js/index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-    const menuBtn = document.querySelector('.menu-btn');
-    const leftMenu = document.querySelector('.left-menu');
-    menuBtn.addEventListener('click', () => {
-        leftMenu.classList.toggle('show');
-    });
-});
