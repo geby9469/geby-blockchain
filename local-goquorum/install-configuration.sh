dfe5af192b5ccfcb0ab0e06a4b2b27045cd8e793
geby <bjk9406@gmail.com>
2022-09-01 12:23:11 +0900

    local quorum blockchain configuration

---
+++ local-goquorum/install-configuration.sh
@@ -0,0 +1,26 @@
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# install nodejs
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install -y yarn
+
+
+# install truffle
+sudo npm install -g -y truffle
+
+# update
+sudo apt-get update -y
+
+# install solc
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo apt-get update -y
+sudo apt-get install -y solc
+
+# install quorum-blockchain
+# npx quorum-dev-quickstart
---
+++ local-goquorum/ubuntu-package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install systemd
+
+# ssh
+sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+# network tool
+sudo apt-get install net-tools
+
+# editor
+sudo apt-get install vim
+
+# public-key
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 0EBFCD88
